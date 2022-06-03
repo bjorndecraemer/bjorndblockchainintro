ad667eee56014204355deea7670047b77c08b5b7
bjorn <bjorn.decraemer@yahoo.com>
2022-06-03 16:15:07 +0200

    Same value Strings give the same hash

--- src/bjornspetprojects/blockchain/BjornsBlockchain.java
+++ src/bjornspetprojects/blockchain/BjornsBlockchain.java
@@ -6,7 +6,10 @@ public class BjornsBlockchain {
         System.out.println("This is a blockchain demo app");
         String toHashValue1 = "This is a value";
         String toHashValue2 = "This is a value";
-        String toHashValue3 = "This is a value";
-        System.out.println();
+        String toHashValue3 = "This is another value";
+        System.out.println("Hashes of same strings are identical");
+        System.out.println("Value1 : "+toHashValue1 + " with hash : "+toHashValue1.hashCode());
+        System.out.println("Value1 : "+toHashValue2 + " with hash : "+toHashValue2.hashCode());
+        System.out.println("Value1 : "+toHashValue3 + " with hash : "+toHashValue3.hashCode());
     }
 }
