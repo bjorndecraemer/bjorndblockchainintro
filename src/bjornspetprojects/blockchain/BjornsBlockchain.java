996d30549dd1c9ccd1b326eed1d97483ed47b09f
bjorn <bjorn.decraemer@yahoo.com>
2022-06-03 16:20:02 +0200

    Same value String arrays give the same hash

--- src/bjornspetprojects/blockchain/BjornsBlockchain.java
+++ src/bjornspetprojects/blockchain/BjornsBlockchain.java
@@ -1,5 +1,7 @@
 package bjornspetprojects.blockchain;
 
+import java.util.Arrays;
+
 public class BjornsBlockchain {
 
     public static void main(String[] args){
@@ -11,5 +13,18 @@ public class BjornsBlockchain {
         System.out.println("Value1 : "+toHashValue1 + " with hash : "+toHashValue1.hashCode());
         System.out.println("Value1 : "+toHashValue2 + " with hash : "+toHashValue2.hashCode());
         System.out.println("Value1 : "+toHashValue3 + " with hash : "+toHashValue3.hashCode());
+
+        // Arrays of Strings also give the same hash values
+        System.out.println("--------------------------------------");
+
+        String[] toHashArray1 = new String[]{toHashValue1,toHashValue3};
+        String[] toHashArray2 = new String[]{toHashValue2,toHashValue3};
+        String[] toHashArray3 = new String[]{toHashValue2,toHashValue1};
+
+        System.out.println("Hashes of same value String Arrays also give the same hash");
+        System.out.println("Array 1"+toHashArray1.toString()+" with hash : "+ Arrays.hashCode(toHashArray1));
+        System.out.println("Array 2"+toHashArray2.toString()+" with hash : "+ Arrays.hashCode(toHashArray2));
+        System.out.println("Array 3"+toHashArray3.toString()+" with hash : "+ Arrays.hashCode(toHashArray3));
+
     }
 }
