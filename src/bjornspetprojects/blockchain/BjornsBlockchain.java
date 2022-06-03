f524aa2b4182a09c11d8f38d86a46b857747b2c6
Bjorn <bjorn@be-c02y83t1jg5j.ordina.belgium>
2022-06-03 16:12:24 +0200

    Initial commit

---
+++ src/bjornspetprojects/blockchain/BjornsBlockchain.java
@@ -0,0 +1,12 @@
+package bjornspetprojects.blockchain;
+
+public class BjornsBlockchain {
+
+    public static void main(String[] args){
+        System.out.println("This is a blockchain demo app");
+        String toHashValue1 = "This is a value";
+        String toHashValue2 = "This is a value";
+        String toHashValue3 = "This is a value";
+        System.out.println();
+    }
+}
